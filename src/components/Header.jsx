0dcf4c51e31e794b99a5796c918c99b3295771ad
SamiRose1 <seimyemane8@gmail.com>
2023-07-21 20:33:18 +0300

    added components/pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Image src={"image"} />
+      <div>
+        <button>About</button>
+        <button>Contact</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
