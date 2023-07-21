a5f2fc6b7a3be9820999633f7d6ec3406f1a82dd
SamiRose1 <seimyemane8@gmail.com>
2023-07-21 22:42:52 +0300

    added a few styles to the Header.jsx and Hero.jsx components, design still needs more work

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import HeroPng from "../images/hero.jpg";
 const Hero = () => {
   return (
     <div className="h-screen flex justify-around items-center bg-sky-100">
-      <div className=" justify-center items-center table-row ">
-        <h1>Welcome!</h1>
+      <div className="flow-root  justify-around items-center w-1/2 lg:text-lg md:text-md sm:text-sm text-center px-10 py-10 h-3/5 bg-slate-400 text-white rounded-lg shadow-lg  shadow-slate-800 my-7">
+        <h1 className="text-2xl lg:text-3xl md:text-xl my-5 ">Welcome!</h1>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis vero
           laborum similique eaque voluptates eius impedit nobis quaerat cum?
